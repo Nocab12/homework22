ee9ba49437da89df4f07dbf4f05e47a27b7eef78
Denis Berezin <nocab@yandex.ru>
2019-05-13 23:47:29 +0300

    lvl2.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,8 @@
-function bonus () {
-let iterable = [200, 550, 4000, 23, 58, 5000, 485, 711];
-let sum = 0;
-for (let value of iterable) {
-sum += value;  
-}
-if (sum > 10) {
-sum = sum / 100 * 5;
-}
-console.log(Math.round(sum));
-}
-bonus();
+const characters = [
+  {name: 'мечник', health: 10},
+  {name: 'маг', health: 100},
+  {name: 'маг', health: 0},
+  {name: 'лучник', health: 0},
+];
+
+const alive = characters.filter(item => item.health > 0);
